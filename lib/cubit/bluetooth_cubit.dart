095dc5ebfe92d53a79b74b4685bb6dfa33837454
nada <nada50tarek@gmail.com>
2023-11-29 18:36:23 +0200

    creates the states of bluetooth

--- lib/cubit/bluetooth_cubit.dart
+++ lib/cubit/bluetooth_cubit.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:meta/meta.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'bluetooth_state.dart';
 
 class BluetoothCubit extends Cubit<BluetoothState> {
--- lib/cubit/bluetooth_state.dart
+++ lib/cubit/bluetooth_state.dart
@@ -4,3 +4,12 @@ part of 'bluetooth_cubit.dart';
 sealed class BluetoothState {}
 
 final class BluetoothInitial extends BluetoothState {}
+
+final class CkeckBluetoothConnection extends BluetoothInitial {}
+
+final class BluetoothScanDevice extends BluetoothInitial {
+  final List<ScanResult> devices;
+  BluetoothScanDevice(this.devices);
+}
+
+final class ReadBluetoothData extends BluetoothInitial {}
